94bda60ae184192036774e21223ca7d39360a666
aPEDESTRIAN <>
2022-11-17 19:24:37 -0800

    ellipsis-throbber css classes animation was in  the wrong order; FIXED!

--- build/infinite-scroll-loading/editor.asset.php
+++ build/infinite-scroll-loading/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '66ea49127cc05d9493e2');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => 'a7acc65dddcc819beac0');
